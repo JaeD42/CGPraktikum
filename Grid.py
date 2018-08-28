c9eb62a74d64fbac13307a342462614e1f5b0599
Jana B <bejana@students.uni-mainz.de>
2018-08-28 10:26:16 +0200

    grid?!

---
+++ Grid.py
@@ -0,0 +1,17 @@
+
+class Grid:
+
+    def __init__(self):
+        pass
+
+
+    def get_point_at_pos(self,pos):
+        pass
+
+    def add_point_at_pos(self,point,pos):
+        pass
+
+    def remove_point_at_pos(self,pos):
+        pass
+
+    
